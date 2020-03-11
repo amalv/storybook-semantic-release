6a8a753d018056aacd62413615ea3b6f7fd16881
Amaro Mariño <1252707+amalv@users.noreply.github.com>
2020-03-11 13:00:31 +0100

    feat: Add types to button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,11 @@
+import React from 'react';
 import styled from "styled-components";
 
-export const Button = styled.button`
+export interface ButtonProps {
+  primary?: boolean
+}
+
+export const Button = styled.button<ButtonProps>`
   /* Adapt the colors based on primary prop */
   background: ${props => props.primary ? "palevioletred" : "white"};
   color: ${props => props.primary ? "white" : "palevioletred"};
